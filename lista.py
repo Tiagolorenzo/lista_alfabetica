b77f30256f4ec17bfcfa45fc7fdc979200c68441
Tiagolorenzo <Tiago.lorenzo@hotmail.com>
2024-05-23 20:26:40 -0300

    1.0

---
+++ lista.py
@@ -0,0 +1,9 @@
+# lista
+nomes = ['alex', 'simone', 'bernardo', 'césar', 'alexandra']
+
+# ordenando a lista. nomes.sort(reverse=True) para ordem inversa
+nomes.sort()
+
+# exibe a lista na tela
+for nome in nomes:
+    print(nome)
